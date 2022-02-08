88f39ab7b46ffc9de559de1a7bb38466b7616ed2
S-Osman4 <94788755+S-Osman4@users.noreply.github.com>
2022-02-08 20:14:50 +0300

    Create CaesarsCipher.js

---
+++ JavaScript Algorithms And Data Structures/CaesarsCipher.js
@@ -0,0 +1,23 @@
+/*
+Caesars Cipher
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+*/
+function rot13(str) {
+  let descipher = str.split('')
+    for (let i=0; i<descipher.length;i++) {
+       var numAscii = (descipher[i].charCodeAt(0) + 13) > 90 ? descipher[i].charCodeAt(0) -13 :  descipher[i].charCodeAt(0) + 13; 
+        if (descipher[i].charCodeAt(0) >= 65 && descipher[i].charCodeAt(0) <= 90) {
+            descipher[i] = String.fromCharCode(numAscii);
+            
+        }
+    }
+    return descipher.join('');
+}
+
+rot13("SERR PBQR PNZC");
